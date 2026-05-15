a3b412fa432da0aa010a73d267aaf96c23267a11
kdongd <kimdongd2@naver.com>
2026-05-15 12:47:05 +0900

    feat: CORS 설정 추가
    - 프론트와 백엔드 포트가 달라 브라우저가 요청을 차단함
    - 모든 출처에서의 요청을 허용하도록 @CrossOrigin 추가

--- src/main/java/com/kdongd/norunnolife/controller/WorkoutController.java
+++ src/main/java/com/kdongd/norunnolife/controller/WorkoutController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class WorkoutController {
 
     private final WorkoutService workoutService;
