3a7a9a6e9e34f0eddcddccb9ebdbc8e1a85b97fe
kdongd <kimdongd2@naver.com>
2026-05-19 17:29:42 +0900

    chore: .DS_Store 삭제 및 .gitignore 추가, refactor: duration 필드명을 durationMinutes로 변경

--- src/main/java/com/kdongd/norunnolife/domain/Workout.java
+++ src/main/java/com/kdongd/norunnolife/domain/Workout.java
@@ -6,6 +6,8 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
+
+
 @Getter
 @Setter
 public class Workout {
@@ -16,7 +18,7 @@ public class Workout {
     private WorkoutType type;
 
     @Min(value = 1, message = "운동 시간은 1분 이상이어야 합니다.")
-    private int duration;
+    private int durationMinutes;
 
     @NotBlank(message = "메모는 비어있을 수 없습니다.")
     private String memo;
